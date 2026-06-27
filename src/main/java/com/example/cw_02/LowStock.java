a643685db10e0e95a2fb625acf5375c8d54447e7
anuja-jayaweera <anuja.bharatha@gmail.com>
2026-06-27 13:43:41 +0530

    Added LowStock.java file to moniter low stock

---
+++ src/main/java/com/example/cw_02/LowStock.java
@@ -0,0 +1,34 @@
+package com.example.cw_02;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LowStock {
+    public static void main(String[] args){
+        String input = "inventory_cleaned.txt";
+
+        try(BufferedReader Bread = new BufferedReader(new FileReader(input))) {
+            String Line;
+            while ((Line = Bread.readLine()) != null) {
+                if (Line.trim().isEmpty()) continue;
+
+                String[] data = Line.split("\\|");
+
+                String code = data[0];
+                String item = data[1];
+                int quantity = Integer.parseInt(data[4]);
+                int threshold = Integer.parseInt(data[8]);
+
+                if (quantity < threshold) {
+                    System.out.printf("WARNING!! Item %s (%s) is in low stock! Current: %d | Threshold: %d \n",
+                            code, item, quantity, threshold);
+                }
+
+            }
+        }
+        catch (IOException e){
+            System.err.println("Error! file not found!");
+        }
+    }
+}
