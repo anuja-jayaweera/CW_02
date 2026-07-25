0bddfa7ba4987bbf89a775a7c8462fe2b0fbb686
anuja-jayaweera <anuja.bharatha@gmail.com>
2026-07-25 20:42:48 +0530

    Updated the Main Controller class

--- src/main/java/com/example/cw_02/Controllers/MainController.java
+++ src/main/java/com/example/cw_02/Controllers/MainController.java
@@ -1,116 +1,47 @@
 package com.example.cw_02.Controllers;
 
 import javafx.event.ActionEvent;
-
-import com.example.cw_02.classes.InventoryStore;
-import com.example.cw_02.classes.PartItem;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 import java.io.IOException;
 
 public class MainController {
-
-    @FXML
-    private TableView<PartItem> inventoryTable;
-
-    @FXML
-    private TableColumn<PartItem, String> colCode;
-    @FXML
-    private TableColumn<PartItem, String> colName;
-    @FXML
-    private TableColumn<PartItem, String> colBrand;
-    @FXML
-    private TableColumn<PartItem, Double> colPrice;
-    @FXML
-    private TableColumn<PartItem, Integer> colStock;
-    @FXML
-    private TableColumn<PartItem, String> colCategory;
-    @FXML
-    private TableColumn<PartItem, String> colImage;
-    @FXML
-    private TableColumn<PartItem, Integer> colStatus;
-
-    private InventoryStore inventoryStore = new InventoryStore();
-
-    @FXML
-    public void initialize() {
-        colCode.setCellValueFactory(new PropertyValueFactory<>("partCode"));
-        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
-        colBrand.setCellValueFactory(new PropertyValueFactory<>("brand"));
-        colPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
-        colStock.setCellValueFactory(new PropertyValueFactory<>("stock"));
-        colCategory.setCellValueFactory(new PropertyValueFactory<>("category"));
-        colImage.setCellValueFactory(new PropertyValueFactory<>("imageFile"));
-        colStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
-
-        loadInventoryData();
-    }
-
-    public void loadInventoryData() {
-        ObservableList<PartItem> items = FXCollections.observableArrayList(inventoryStore.viewInventory());
-        inventoryTable.setItems(items);
-    }
-
-    @FXML
-    public void openAddItemsWindow(ActionEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/cw_02/add-item-view.fxml"));
-            Parent root = loader.load();
-            Stage stage = new Stage();
-            stage.setTitle("Add New Item");
-            stage.setScene(new Scene(root));
-
-            stage.showAndWait();
-            loadInventoryData();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error Loading View");
-            alert.setContentText("Could not load view: add-item-view.fxml");
-            alert.showAndWait();
-        }
-    }
-
-    public void openDeleteItemsWindow(ActionEvent event) {
+    private void openWindow( String fxmlFile, String title){
         try{
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/cw_02/delete-items-view.fxml"));
-            Parent root = loader.load();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/cw_02/" + fxmlFile));
+            Parent root = fxmlLoader.load();
             Stage stage = new Stage();
-            stage.setTitle("Delete Item from Inventory");
+            stage.setTitle(title);
             stage.setScene(new Scene(root));
-
-            stage.showAndWait();
-            loadInventoryData();
-        }catch (IOException e) {
+            stage.show();
+        }catch (IOException e){
             e.printStackTrace();
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error Loading View");
-            alert.setContentText("Could not load view: delete-item-view.fxml");
-            alert.showAndWait();
+            System.err.println("ERROR! Could not load the window: " + title);
         }
     }
 
-    // 2. MAKE SURE THIS METHOD IS PUBLIC AND USES javafx.event.ActionEvent
     @FXML
-    public void promptToAddItems(ActionEvent event) {
-        openAddItemsWindow(event);
+    public void openAddItem(ActionEvent event){
+        openWindow("add-item-view.fxml","Add new Item");
     }
 
     @FXML
-    public void promptToDeleteItems(ActionEvent event){
-        openDeleteItemsWindow(event);
+    public void openDeleteItem(ActionEvent event){
+        openWindow("delete-items-view.fxml","Delete Item");
+    }
+
+    @FXML
+    public void openUpdateItem(ActionEvent event){
+        openWindow("update-item-view.fxml","Update Item Details");
+    }
+
+    @FXML
+    public void openDataCleaner(ActionEvent event){
+        openWindow("data-cleaner-view.fxml","Clean Legacy text files");
     }
 
 }
